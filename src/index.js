0de8072668b4c4b5f7c3a2c3776c2aefc5afe770
Lynh <chinsugaming@gmail.com>
2023-11-17 21:45:16 +0700

    mongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001
+
+
+mongoose.connect('mongodb://0.0.0.0:27017/botstoreMERN')
+    .then(() => {
+        console.log("Ket not thanh cong")
+    }).catch((err) => {
+        console.log(err)
+    });
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World !!!!!!')
